9338b7700b66f6511f4a1704c44df503e82f0354
Rogerio Matoso <rogeriomatoso@yahoo.com.br>
2020-10-29 14:35:00 -0300

    Inicio da Implemmentação da camada Repository.

---
+++ Mecanica.Autos.Repositorio.Comum/IRepositorioGenerico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mecanica.Autos.Repositorios.Comum
+{
+    public interface IRepositorioGenerico<TEntidade, Tchave>
+        where TEntidade : class
+    {
+        List<TEntidade> Selecionar();
+        TEntidade SelecionarPorId(Tchave id);
+        void Inserir(TEntidade enntidade);
+        void Alterar(TEntidade entidade);
+        void Excluir(TEntidade entidade);
+        void ExcluirPorId(Tchave id);
+
+    }
+}
---
+++ Mecanica.Reposittorios.Comum.Entity/RepositorioGenericoEntity.cs
@@ -0,0 +1,43 @@
+﻿using Mecanica.Autos.Repositorios.Comum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mecanica.Repositorios.Comum.Entity
+{
+   public class RepositorioGenericoEntity<TEntidade, TChave> : IRepositorioGenerico<TEntidade, TChave>
+        where TEntidade : class
+    {
+        public void Alterar(TEntidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(TEntidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirPorId(TChave id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(TEntidade enntidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntidade> Selecionar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntidade SelecionarPorId(TChave id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
